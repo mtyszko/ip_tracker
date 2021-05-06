e9b9b826ff763db409c4a48989b34241dc65b9f8
mtyszko <michal.tyszko@gmail.com>
2021-05-06 09:54:19 +0200

    handelSearch
    add handleSearch funcionalities - set state: data & err msg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Main from 'layouts/Main';
 
 import './App.css';
+
+import { ipifyBaseUrl } from 'utils/vars';
 
 export const GlobalContext = React.createContext();
 class App extends React.Component {
@@ -14,15 +17,46 @@ class App extends React.Component {
     locName: '',
     lat: '',
     lng: '',
-    tzone: '',
+    timezone: '',
     isp: '',
+
     handleInput: (e) => {
       this.setState({
         input: e.target.value,
       });
     },
-    handleSearch: (e) => {
+
+    handleSearch: async (e) => {
+      this.setState({ loading: true });
       e.preventDefault();
+      console.log(this.state.input);
+      try {
+        const res = await axios.get(`${ipifyBaseUrl}${this.state.input}`);
+
+        const {
+          data: {
+            ip,
+            location: { city, lat, lng, region, timezone },
+            isp,
+          },
+        } = res;
+
+        this.setState({
+          loading: false,
+          err: '',
+          ip,
+          locName: `${city}, ${region}`,
+          lat,
+          lng,
+          timezone,
+          isp,
+        });
+      } catch (err) {
+        this.setState({
+          loading: false,
+          err: 'Input correct IPv4 or IPv6 address.',
+        });
+      }
     },
   };
 
