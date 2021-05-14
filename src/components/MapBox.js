58420d9ea8709bad65fb91c978e8757b79a5232e
mtyszko <michal.tyszko@gmail.com>
2021-05-14 11:42:46 +0200

    add MapBox componment

---
+++ src/components/MapBox.js
@@ -0,0 +1,56 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { DataContext } from 'App';
+
+import icon from 'assets/icon-location.svg';
+
+const accessToken = process.env.REACT_APP_LMAPS_API_KEY;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Map = ReactMapboxGl({
+  accessToken,
+  zoom: 15,
+});
+
+export default function MapBox() {
+  const {
+    data: { lng, lat },
+  } = useContext(DataContext);
+
+  return (
+    <Container>
+      <Map
+        style='mapbox://styles/mapbox/streets-v9' // eslint-disable-line
+        containerStyle={{
+          height: '100%',
+          width: '100%',
+        }}
+        zoom={[14]}
+        center={[lng, lat]}
+      >
+        <Layer
+          type='symbol'
+          id='marker'
+          llayout={{ 'icon-image': 'marker-15' }}
+        >
+          <Feature coordinates={[lat, lng]} />
+        </Layer>
+
+        <Marker coordinates={[lng, lat]} anchor='bottom'>
+          <img src={icon} alt='map' />
+        </Marker>
+      </Map>
+    </Container>
+  );
+}
+
+Map.propTypes = {};
