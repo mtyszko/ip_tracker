79cad48bd42874d90d1add55e12d3d0fe19fcf12
mtyszko <michal.tyszko@gmail.com>
2021-05-17 13:58:56 +0200

    add loading to page structure

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import Search from 'components/Search';
+import DataPanel from 'components/DataPanel';
 import MapBox from 'components/MapBox';
+import Footer from 'components/Footer';
 
 import bgimg from 'assets/pattern-bg.png';
 
@@ -25,6 +27,14 @@ const Header = styled.header`
   }
 `;
 
+const Title = styled.h1`
+  margin: 0 auto;
+  padding: 30px 0 20px;
+  color: #ffffff;
+  font-size: clamp(24px, 3vw, 28px);
+  text-align: center;
+`;
+
 const MapWrapper = styled.section`
   height: 100%;
   background-color: mediumaquamarine;
@@ -33,11 +43,14 @@ const MapWrapper = styled.section`
 const Main = () => (
   <Wrapper>
     <Header>
+      <Title>IP Adress Tracker</Title>
       <Search />
+      <DataPanel />
     </Header>
     <MapWrapper>
       <MapBox />
     </MapWrapper>
+    <Footer />
   </Wrapper>
 );
 
